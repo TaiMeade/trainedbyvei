f24452f54c32592bfeb62c142ff23b65cec42a1f
Tai Meade <taitaishi123@gmail.com>
2025-11-22 03:10:10 -0500

    added send email logic (seems to be working now)

--- src/components/ContactDialog.vue
+++ src/components/ContactDialog.vue
@@ -7,11 +7,13 @@
 
       <v-row justify="center" class="px-4" >
         <v-col cols="12">
-          <v-form>
-            <v-text-field label="Name" required></v-text-field>
-            <v-text-field label="Email" required></v-text-field>
-            <v-textarea label="Message" rows="4"></v-textarea>
-            <v-btn class="mt-4" id="send-msg-btn">Send</v-btn>
+          <v-form @submit.prevent="sendEmail">
+            <v-text-field v-model="name" label="Name" required></v-text-field>
+            <v-text-field v-model="email" label="Email" required></v-text-field>
+            <v-textarea v-model="message" label="Message" rows="4"></v-textarea>
+            <v-btn :loading="loading" type="submit" class="mt-4" id="send-msg-btn">Send</v-btn>
+            <div v-if="success" style="color: green;">Message sent!</div>
+            <div v-if="error" style="color: red;">Failed to send. Try again.</div>
           </v-form>
         </v-col>
       </v-row>
@@ -28,6 +30,43 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import emailjs from '@emailjs/browser';
+
+const name = ref('')
+const email = ref('')
+const message = ref('')
+const loading = ref(false)
+const success = ref(false)
+const error = ref(false)
+
+const SERVICE_ID = 'trainedbyvei'
+const TEMPLATE_ID = 'template_17nzpmp'
+const PUBLIC_KEY = 'IF2mEdPZwwhrt0v4e'
+
+function sendEmail() {
+  loading.value = true
+  error.value = false
+  success.value = false
+
+  emailjs.send(SERVICE_ID, TEMPLATE_ID, {
+    name: name.value,
+    email: email.value,
+    message: message.value
+  }, PUBLIC_KEY)
+    .then(() => {
+      loading.value = false
+      success.value = true
+      name.value = ''
+      email.value = ''
+      message.value = ''
+    })
+    .catch(() => {
+      loading.value = false
+      error.value = true
+    })
+}
+
 defineProps({
   modelValue: Boolean
 })
