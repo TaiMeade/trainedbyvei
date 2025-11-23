d9991ac3c1f18ff205ec706049c8472c96bef9e7
Tai Meade <taitaishi123@gmail.com>
2025-11-23 17:55:25 -0500

    attempting to move images to assets folder

--- src/components/TransformationCarousel.vue
+++ src/components/TransformationCarousel.vue
@@ -19,13 +19,13 @@ import Transformation from './Transformation.vue'
 const transformations = [
   {
     title: 'Matvei Isbell',
-    before: `./images/matvei1.jpg`,
-    after: `./images/matvei2.jpg`
+    before: `./src/assets/matvei1.jpg`,
+    after: `./src/assets/matvei2.jpg`
   },
   {
     title: 'Silar Moore',
-    before: `./images/silar1.jpg`,
-    after: `./images/silar3.jpg`
+    before: `./src/assets/silar1.jpg`,
+    after: `./src/assets/silar3.jpg`
   }
 ]
 </script>
