6dfbb17cf12fc545d45625c1f772a38c9cee18f8
Tai Meade <taitaishi123@gmail.com>
2025-11-24 22:23:10 -0500

    updated footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,8 @@
 <template>
   <footer class="footer">
     <div class="footer-content">
-      <span>&copy; {{ new Date().getFullYear() }} Trained by Vei</span>
+      <span>&copy; {{ new Date().getFullYear() }} | Trained by Vei | All Rights Reserved</span>
+      <span>Developed by <a class="no-style-link" target="_blank" href="https://github.com/TaiMeade">taimeade</a></span>
       <div class="footer-links">
         <a href="https://www.instagram.com/veitrains" target="_blank" rel="noopener" aria-label="Instagram">
           <i class="fab fa-instagram"></i>
@@ -25,19 +26,32 @@
   padding: 1rem 0;
   text-align: center;
 }
+
 .footer-content {
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 0.5rem;
 }
+
 .footer-links a {
   color: #fff;
   margin: 0 0.5rem;
   font-size: 1.5rem;
   transition: color 0.2s;
 }
+
 .footer-links a:hover {
   color: #ff9800;
+}
+
+.no-style-link {
+  color: inherit;
+  text-decoration: none;
+}
+
+.no-style-link:hover {
+  color: lightblue;
+  text-decoration: underline;
 }
 </style>
